ba956121aae2eed6afa8645c33c19e07d8acd904
Kristo <116211837+peskybirb@users.noreply.github.com>
2025-12-19 19:04:22 +0200

    Ülesanne 3 (kodutöö)
    Kodutöö mis anti enne jõule. Kasutades "&&" et saaks kontrollida 2 asja korraga.

---
+++ Kodutöö ülesanne3 Kristo Ruus LOGITge25/Ülesanne 3/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string KasutajaNimi = "";
+        string KasutajaParool = "";
+      
+
+        Console.WriteLine("Palun sisesta oma kasutajanimi");
+        KasutajaNimi = Console.ReadLine();
+
+        Console.WriteLine("palun sisesta oma parool");
+        KasutajaParool = Console.ReadLine();
+
+        if (KasutajaNimi == "User1" && KasutajaParool == "qwerty")
+        {
+            Console.WriteLine("Tere tulemast "+KasutajaNimi+", olete edukalt sisse logitud");  
+        }
+        else
+        {
+            Console.WriteLine("Kasutajanimi või parool pole õige, Sissepääs on keelatud");
+        }
+}
+}
