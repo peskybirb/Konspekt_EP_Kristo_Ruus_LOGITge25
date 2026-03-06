3c00d2f9fdbde7ed969688fec916cad9967adfb4
Opilane TTHK <116211837+peskybirb@users.noreply.github.com>
2026-03-06 15:29:14 +0200

    File Writer ül prooviks

---
+++ Meetodid/FileWriterTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FileWriterTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            if (File.Exists("KasutajaNimi.txt"))
+            {
+                string[] tervitus = File.ReadAllLines("kasutajaNimi.txt");
+                Console.WriteLine(tervitus[0]);
+            }
+            else
+            {
+                Console.WriteLine("mis on su nimi");
+                string vastus = Console.ReadLine();
+                string vaste = "Tervetulemast " + vastus;
+                File.WriteAllText("KasutajaNimi.txt", vaste);
+
+            }
+        }
+    }
+}
