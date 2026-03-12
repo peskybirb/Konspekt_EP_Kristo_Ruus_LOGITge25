fa88c41c328b240d22532888103c811c920b467a
Opilane TTHK <116211837+peskybirb@users.noreply.github.com>
2026-03-12 15:03:51 +0200

    Player Stats
    Ma eriti aru ei saa kuidas see töötab aga peaks phm töötama , eksisteerib struct Location ja Player klass kus on kõik statid koos

---
+++ Kontrolltöö/Meetodite töö/Adventure/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure
+{
+    public class Player
+    {
+        public struct Location
+        {
+            public Location(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public int? X { get; set; } = 0;
+            public int? Y { get; set; } = 0;
+
+        }
+
+        public Player(
+            int lives,
+            int health,
+            int money,
+            List<string> backpack,
+            Location playerLocation
+            )
+        {
+            Lives = lives;
+            Heatlh = health;
+            Money = money;
+            Backpack = backpack;
+            PlayerLocation = playerLocation;
+        }
+        public int? Lives { get; set; } = 3;
+        public int? Heatlh { get; set; } = 100;
+        public int Money { get; set; }
+        public List<string> Backpack { get; set; }
+        public Location PlayerLocation { get; set; }
+
+    }
+}
---
+++ Kontrolltöö/Meetodite töö/Adventure/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Adventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  1. tee player klass koos viie andmeväljaga 
+            //  2. player klassis on 1 konstruktor kus kasutatakse kõiki andmeid
+            //  andmeväljad on , Lives,Health,Location (Struct kus on X ja Y), backpack , money 
+            //  vaikeväärtused on Lives = 3 , Healt = 100
+        }
+    }
+}
