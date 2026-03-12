433359da94d2a1a995ec60cbe080df5c8f0ce8fd
Opilane TTHK <116211837+peskybirb@users.noreply.github.com>
2026-03-12 14:27:46 +0200

    ...

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Class1(string messageasstring)
+        {
+            MsgForUser = messageasstring;
+        }
+        public string MsgForUser { get; set; } = "HEllololo";
+        public void WhatsTheWord()
+        {
+            Console.WriteLine(MsgForUser);
+        }
+    }
+
+}
